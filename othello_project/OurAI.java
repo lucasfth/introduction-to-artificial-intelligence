989f8e495240b76360abf602241a967b1b208c25
Lucas Frey Torres Hanson <lucasfth@bjelke-torres.com>
2024-03-16 21:35:49 +0100

    Add print statement to help visualize
    Also one problem is that the correct value is not returned.
    I suspect it is due to line 83 - 88

--- othello_project/OurAI.java
+++ othello_project/OurAI.java
@@ -8,29 +8,31 @@ public class OurAI implements IOthelloAI {
 	 */
  @Override
 	public Position decideMove(GameState s){
-		System.out.println("Init board:\n" + Arrays.deepToString(s.getBoard()));
+		// System.out.println("Init board:\n" + Arrays.deepToString(s.getBoard()));
+		System.out.println("Init board:\n");
+		printBoard(s.getBoard());
 
-		System.out.println("OurAI is thinking...");
-	
-		System.out.println("Legal moves for " + s.getPlayerInTurn() + ": " + s.legalMoves().size());
+		System.out.println("Amount of legal moves for " + (isBlackTurn(s) ? "black" : "white") + " : " + s.legalMoves().size());
 
 		ArrayList<Position> moves = s.legalMoves();
 		int bestMove = 0;
 		Position bestPosition = moves.get(0);
 
 		for (Position p : moves) {
-			System.out.println("--------- Trying outer position thing --------");
+			System.out.println("--- Outloop iteration ---");
 
 			int[][] newBoard = getNewBoard(s, p);
-			System.out.println("\t above new board called from outer loop");
+
 			GameState ns = getNewGameState(s, newBoard);
-			
+
 			Tuple<Position, Integer, Integer> result = minimax(ns, 0);
+			System.out.println("Result from minimax: " + result.getValue() + " and pos: " + result.getPosition());
 			if (result.getValue() > bestMove) {
 				bestMove = result.getValue();
 				bestPosition = result.getPosition();
 				System.out.println("**************** Best position ****************\n" + bestPosition);
 			}
+			System.out.println("--- End of outloop iteration ---\n\n");
 		}
 		System.out.println("OurAI has decided a move");
 		return bestPosition;
@@ -46,20 +48,30 @@ public class OurAI implements IOthelloAI {
 
 		for (Position p: moves) {
 			int[][] newBoard = getNewBoard(s, p);
-			System.out.println("\t new board called from minimax... " + Arrays.deepToString(newBoard));
 			GameState ns = getNewGameState(s, newBoard);
-			System.out.println("Create new Game state ns: " + Arrays.deepToString(ns.getBoard()));
+
 			if (ns.isFinished()) {
 				int winner = findWinner(ns);
+				System.out.println("\t\tFound winning state for: " + (isBlackTurn(ns) ? "black" : "white"));
+				System.out.println("\t\tWill return pos: " + p + " with winner: " + winner + " and depth: " + depth);
 				return new Tuple<Position, Integer, Integer>(p, winner, depth);
 			}
 
 			if (depth == 2) {
-				return getBestDepthMove(s, ns);
+				System.out.println("\t\tReached max depth for: " + (isBlackTurn(ns) ? "black" : "white"));
+
+				Tuple<Position, Integer, Integer> ret = getBestDepthMove(s, ns);
+				System.out.println("\t\tWill return pos: " + p + " with winner: " + ret.getValue() + " and depth: " + depth);
+				return ret;
 			}
 
 			// Call minimax recursively with mimicked game state
 			Tuple<Position, Integer, Integer> recursive = minimax(ns, depth);
+
+			System.out.println("\tMaxMove val:   " + maxMove.getValue());
+			System.out.println("\tMinMove val:   " + minMove.getValue());
+			System.out.println("\tRecursive val: " + recursive.getValue());
+			System.out.println("\t" + (isBlackTurn(ns) ? "Black" : "White") + " turn");
 
 			if (maxMove.getValue() < recursive.getValue()  &&  isBlackTurn(ns)) {
 				maxMove = recursive;
@@ -69,8 +81,10 @@ public class OurAI implements IOthelloAI {
 			}
 		}
 		if (isBlackTurn(s)){
+			System.out.println("\tReturning MaxMove: " + maxMove.getValue());
 			return maxMove;
 		}
+		System.out.println("\tReturning MinMove: " + minMove.getValue());
 		return minMove;
 	}
 
@@ -89,38 +103,30 @@ public class OurAI implements IOthelloAI {
 	public int[][] getNewBoard(GameState s, Position p) {
 
 		int[][] originalBoard = s.getBoard();
-		int[][] newBoard = new int[originalBoard.length][];
+		int[][] newBoard = new int[originalBoard.length][originalBoard.length]; // originalBoard.length can be used in both dimensions since it's a square
+
 		for (int i = 0; i < originalBoard.length; i++) {
-			newBoard[i] = Arrays.copyOf(originalBoard[i], originalBoard[i].length);
+			System.arraycopy(originalBoard[i], 0, newBoard[i], 0, originalBoard[i].length);
 		}
 
 		newBoard[p.row][p.col] = isBlackTurn(s) ? 1 : 2;
 
+		printBoard(newBoard);
+
 		return newBoard;
-		// System.out.println("Get board 1: \n" + Arrays.deepToString(s.getBoard()));
-		// int[][] newBoard = s.getBoard();
-		// System.out.println("Get new board: \n" + Arrays.deepToString(newBoard) + " - 1");
-
-		// newBoard[p.row][p.col] = isBlackTurn(s) ? 1 : 2;
-		// System.out.println("p.row: " + p.row + " ::::  p.col: " + p.col);
-		// System.out.println("Get new board:\n" + Arrays.deepToString(newBoard) + " - 2");
-
-		// System.out.println("Creating new board from " + Arrays.deepToString(s.getBoard()) + " to " + Arrays.deepToString(newBoard));
-		// return newBoard;
 	}
-	
+
 	/**
 	 * Returns the best move for the current player
 	 * @param s is the current game state
 	 * @param ns is the next game state
 	 * @return the best move
 	 */
-
 	public Tuple<Position, Integer, Integer> getBestDepthMove(GameState s, GameState ns) {
-	   int[] nsTokens = ns.countTokens();
-      int[] sTokens = s.countTokens();
+		int[] nsTokens = ns.countTokens();
+		int[] sTokens = s.countTokens();
 
-		if (ns.getPlayerInTurn() == 1){
+		if (isBlackTurn(ns)){
 			return new Tuple<Position, Integer, Integer>(null, (nsTokens[0] > sTokens[0] ? nsTokens[0] : sTokens[0]), 0);
 		} else {
 			return new Tuple<Position, Integer, Integer>(null, (nsTokens[1] < sTokens[1] ? nsTokens[1] : sTokens[1]), 0);
@@ -154,13 +160,23 @@ public class OurAI implements IOthelloAI {
 		}
 	}
 
+	public void printBoard(int[][] board) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < board.length; i++) {
+			sb.append(Arrays.toString(board[i]) + "\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
 
 	public class Tuple<P, V, D> {
 		private Position position;
-		private Integer value;
-		private Integer depth;
+		private int value;
+		private int depth;
 
-		public Tuple(Position position, Integer value, Integer depth) {
+		public Tuple(Position position, int value, int depth) {
 			this.position = position;
 			this.value = value;
 			this.depth = depth;
@@ -170,12 +186,12 @@ public class OurAI implements IOthelloAI {
 
 		public Position getPosition() { return position; }
 
-		public void setValue(Integer value) { this.value = value; }
+		public void setValue(int value) { this.value = value; }
 
-		public Integer getValue() { return value; }
+		public int getValue() { return value; }
 
-		public void setDepth(Integer depth) { this.depth = depth; }
+		public void setDepth(int depth) { this.depth = depth; }
 
-		public Integer getDepth() { return depth; }
+		public int getDepth() { return depth; }
 	}
 }
